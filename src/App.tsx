61fa196b438a83d7054145bd7f87b71d0f21d517
BD <oliverluo0723@gmail.com>
2025-08-26 22:42:21 +0800

    安裝router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import "./App.css";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-
 function App() {
-    const [count, setCount] = useState(0);
-
-    const handleClick = (value: number): void => {
-        setCount((prev) => prev + value);
-    };
-
     return (
         <>
-            <h1>{count}</h1>
-            <Button
-                onClick={() => {
-                    handleClick(10);
-                }}
-            >
-                Click me
-            </Button>
+            <h1 className=" text-3xl">Hello</h1>
         </>
     );
 }
